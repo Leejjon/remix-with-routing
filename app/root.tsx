12cfa56d32e69dc4e05858b9974c6e4fc7cac8a5
Leejjon <leejjon@gmail.com>
2024-02-16 14:07:14 +0100

    Merge

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,5 @@
 import { cssBundleHref } from "@remix-run/css-bundle";
-import type { LinksFunction } from "@remix-run/node";
+import type {LinksFunction, LoaderFunctionArgs} from "@remix-run/node";
 import {
   Link,
   Links,
@@ -7,9 +7,10 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
+  ScrollRestoration, useLoaderData,
 } from "@remix-run/react";
 import {getCountryCode, getTranslator} from "~/i18n";
+import {json} from "@remix-run/node";
 
 export async function loader({request}: LoaderFunctionArgs) {
   const t = getTranslator(getCountryCode(request));
